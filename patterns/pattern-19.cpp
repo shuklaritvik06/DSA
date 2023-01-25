cbafef55f3ae4e96b87856ba6cf53864ecf7e400
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-01-25 22:04:44 +0530

    init

--- patterns/pattern-19.cpp
+++ patterns/pattern-19.cpp
@@ -14,23 +14,28 @@ int main()
 {
     int i = 1;
     int j = 1;
+    int k = 1;
     while (i <= 10)
     {
         /* code */
         while (j <= 10)
         {
             /* code */
-            if (i == 1 || i == 10)
+            if (i > 5 ? ((j <= k) || (j >= 11 - k && j <= 10)) : ((j >= 1 && j <= 6 - i) || (j >= 5 + i && j <= 10)))
             {
                 /* code */
                 cout << "*";
             }
-            else if (i >= 2 && i <= 9)
+            else
             {
-                /* code */
+                cout << " ";
             }
             j++;
         }
+        i++;
+        i > 6 ? k++ : k;
+        j = 1;
+        cout << endl;
     }
     return 0;
 }
