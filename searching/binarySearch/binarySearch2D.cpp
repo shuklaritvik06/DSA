7c1a51f70660d245579d19718f294aa52ebda09a
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-01-28 02:58:55 +0530

    init

--- searching/binarySearch/binarySearch2D.cpp
+++ searching/binarySearch/binarySearch2D.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int binarySearch(vector<vector<int>> &arr, int target)
+vector<int> binarySearch(vector<vector<int>> &vect, int target)
 {
-    int size = arr.size();
-    while ()
+    int row = 0;
+    int size = vect.size();
+    int c = size - 1;
+    while (row < size && c >= 0)
     {
+        if (vect[row][c] == target)
+        {
+            return vector<int>{row, c};
+        }
+        if (vect[row][c] < target)
+        {
+            row++;
+        }
+        if (vect[row][c] > target)
+        {
+            c--;
+        }
     }
+
+    return vector<int>{-1, -1};
 }
 int main()
 {
     vector<vector<int>> vect{
-        {1, 234, 23},
-        {3, 4, 5},
-        {78, 44, 24}};
-    binarySearch(vect, 44);
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+    vector<int> result = binarySearch(vect, 4);
+    cout << "Row: " << result[0] << " "
+         << "Col: " << result[1] << endl;
     return 0;
 }
--- sorting/shell.cpp
+++ sorting/shell.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-// O(nlog n)
-// O(n2)
+// O(nlog n) -> Best
+// O(n2) -> Worst
 std::vector<double> sort(std::vector<double> &arr)
 {
     for (int interval = arr.size() / 2; interval > 0; interval /= 2)
@@ -24,8 +24,6 @@ int main()
         1.0, 0.6, 5.8, 2.9, 4, 3, 7, 10, 8};
     std::vector<double> sorted = sort(arr);
     for (int i = 0; i < sorted.size(); i++)
-    {
         std::cout << sorted[i] << " ";
-    }
     return 0;
 }
