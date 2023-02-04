ca11314a9b484578dce627cfa4929799c9baf2c1
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-02-04 22:06:20 +0530

    init

--- recursion/quickFirst.cpp
+++ recursion/quickFirst.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int partition(vector<int> &arr, int start, int end)
 {
-    int pivot = arr[end];
-    int i = start - 1;
-    for (int j = start; j <= end; j++)
+    int pivot = arr[start];
+    int i = start;
+    for (int j = start + 1; j <= end; j++)
     {
         if (arr[j] < pivot)
         {
             i++;
-            swap(arr[j], arr[i]);
+            swap(arr[i], arr[j]);
         }
     }
-    swap(arr[i + 1], arr[end]);
-    return i + 1;
+    swap(arr[i], arr[start]);
+    return i;
 }
 void quickSort(vector<int> &arr, int low, int high)
 {
@@ -27,7 +27,7 @@ void quickSort(vector<int> &arr, int low, int high)
 }
 int main()
 {
-    vector<int> arr{1, 5, 4, 3, 2, 2};
+    vector<int> arr{10, 90, 30, 90, 40, 70, 50, 110, 15, 14, 78, 99, 87, 100};
     quickSort(arr, 0, arr.size() - 1);
     for (auto i : arr)
         cout << i << " ";
