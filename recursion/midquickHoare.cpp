0c5b54fa00da00bb27ac5043c7f826e2446e4104
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-02-05 03:04:02 +0530

    init

--- recursion/midquickHoare.cpp
+++ recursion/midquickHoare.cpp
@@ -3,42 +3,38 @@
 using namespace std;
 int partition(vector<int> &arr, int start, int end)
 {
-    int pivot = arr[start];
     int i = start;
     int j = end;
+    int mid = i + (j - i) / 2;
+    int pivot = arr[mid];
     while (i < j)
     {
         while (arr[i] <= pivot)
-        {
             i++;
-        }
         while (arr[j] > pivot)
-        {
             j--;
-        }
         if (i < j)
-        {
             swap(arr[i], arr[j]);
-        }
     }
-    swap(arr[start], arr[j]);
+    swap(arr[mid], arr[j]);
     return j;
 }
-void hoarePartition(vector<int> &arr, int low, int high)
+void quickSort(vector<int> &arr, int low, int high)
 {
     if (low < high)
     {
         int p = partition(arr, low, high);
-        hoarePartition(arr, low, p - 1);
-        hoarePartition(arr, p + 1, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
     }
 }
 int main()
 {
-    vector<int> arr{10, 7, 8, 9, 5, 4, 3, 2, 6};
-    int n = arr.size();
-    hoarePartition(arr, 0, n - 1);
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    vector<int> arr{10, 30, 15, 18, 20, 28, 48, 27, 10, 10, 23, 237};
+    quickSort(arr, 0, arr.size() - 1);
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
     return 0;
 }
---
+++ recursion/randomINdex.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void quickSort()
+{
+}
+int main()
+{
+    return 0;
+}
