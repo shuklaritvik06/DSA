1602e10b22d2923ab43783567deffe464c9b2c28
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-06-13 23:00:00 +0530

    add quest

--- striver_sde_sheet/arrays/quest-1/best.py
+++ striver_sde_sheet/arrays/quest-1/best.py
@@ -5,11 +5,29 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        pass
-    
-a = Solution()
-a.setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]])
+        col_flag = 1
+        rows, cols = len(matrix), len(matrix[0])        
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    if j == 0:
+                        col_flag = 0
+                    else:
+                        matrix[0][j] = 0
+        
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for j in range(cols):
+                matrix[0][j] = 0
+        
+        if col_flag == 0:
+            for i in range(rows):
+                matrix[i][0] = 0
 
-# 0 1 2 0
-# 3 4 5 2
-# 1 3 1 5
+a = Solution()
+a.setZeroes([[0, 1]])
